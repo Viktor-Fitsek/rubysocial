aed92b2ce7f0b31d4647f74c0b8bf860e7ff460e
Viktor Fitsek <viktor@pop-os.localdomain>
2023-07-13 12:01:11 +0200

    Remove test comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   get 'posts/index'
    #root to: "posts#index"
-# test comment
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
